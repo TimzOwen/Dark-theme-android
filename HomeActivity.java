f61139ab7683437639eabfa8be22b9ed7ab9c21c
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-07-02 20:20:36 -0700

    Create HomeActivity.java
    The java picker to perform the UI actions

---
+++ HomeActivity.java
@@ -0,0 +1,17 @@
+package com.owen.darkmode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+    
+    private Switch mySwitch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
