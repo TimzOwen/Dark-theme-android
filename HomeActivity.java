a09401983bf75ce6af65a9ec45caff65ae9fb870
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-07-05 15:31:45 -0700

    Update HomeActivity.java
    updated the switch functionality

--- HomeActivity.java
+++ HomeActivity.java
@@ -20,5 +20,36 @@ public class MainActivity extends AppCompatActivity {
         
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+             mySwitch = (Switch) findViewById(R.id.switchMode);
+
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
+        {
+            mySwitch.setEnabled(true);
+        }
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b)
+            {
+                if (isChangingConfigurations())
+                {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    restartApp();
+                }
+                else
+                {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    
+                    restartApp();
+                }
+
+            }
+        });
     }
-}
+    
+    public void restartApp()
+    {
+        
+    }
+    }
+
